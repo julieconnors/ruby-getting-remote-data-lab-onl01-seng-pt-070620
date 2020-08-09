77ccadc9d9624573dbe747ef18a6a7f7151d7385
Juliana Connors <juliana.g.connors@gmail.com>
2020-08-09 16:48:35 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,19 @@
-# Write your code here
+require 'net/http'
+
+class GetRequester
+  attr_accessor :url
+  
+  def initialize(url)
+    @url = url
+    
+  end
+  
+  def get_response_body
+
+  end
+  
+  def parse_json
+  
+  end
+  
+end
